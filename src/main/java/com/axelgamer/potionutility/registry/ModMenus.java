5f9bf7fc55f29341e63f9f0512042294fef96473
MVCodeMaster <mouvepoiraxeld@gmail.com>
2025-07-12 18:27:26 -0400

    Modify modmenu

--- src/main/java/com/axelgamer/potionutility/registry/ModMenus.java
+++ src/main/java/com/axelgamer/potionutility/registry/ModMenus.java
@@ -1,10 +1,17 @@
 package com.axelgamer.potionutility.registry;
 
 import com.axelgamer.potionutility.PotionUtility;
+import com.axelgamer.potionutility.registry.blockEntity.PotionInjectorStandBlockEntity;
+import com.axelgamer.potionutility.registry.menu.PotionInjectorStandMenu;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.function.Supplier;
+
 public class ModMenus {
     public static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(Registries.MENU, PotionUtility.MODID);
+
+    public static final Supplier<MenuType<PotionInjectorStandMenu>> POTION_INJECTOR_STAND_MENU = MENU_TYPES.register("potion_injector_stand_menu", () -> new MenuType<>(PotionInjectorStandMenu::new, FeatureFlags.DEFAULT_FLAGS));
 }
--- src/main/java/com/axelgamer/potionutility/registry/menu/PotionInjectorStandMenu.java
+++ src/main/java/com/axelgamer/potionutility/registry/menu/PotionInjectorStandMenu.java
@@ -1,14 +1,27 @@
 package com.axelgamer.potionutility.registry.menu;
 
+import com.axelgamer.potionutility.registry.ModMenus;
+import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.item.ItemStack;
-import org.jetbrains.annotations.Nullable;
 
 public class PotionInjectorStandMenu extends AbstractContainerMenu {
-    protected PotionInjectorStandMenu(@Nullable MenuType<?> menuType, int containerId) {
-        super(menuType, containerId);
+    private Container potionInjectorStand;
+    private ContainerData potionInjectorStandData;
+
+    public PotionInjectorStandMenu(int containerId, Inventory playerInventory) {
+        this(containerId, playerInventory, new SimpleContainer(4), new SimpleContainerData(2));
+    }
+
+    protected PotionInjectorStandMenu(int containerId, Inventory playerInventory, Container blockContainer, ContainerData blockContainerData) {
+        super(ModMenus.POTION_INJECTOR_STAND_MENU.get(), containerId);
+        this.potionInjectorStand = blockContainer;
+        this.potionInjectorStandData = blockContainerData;
     }
 
     @Override
